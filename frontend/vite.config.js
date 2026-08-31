794cdc53fee304ff063e9a5256a6e6c57d61d11b
Lohith909 <lohithtallapudi909@gmail.com>
2026-08-31 16:44:28 +0530

    Expose CloudCostOps through ALB

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3000,
 
+    allowedHosts: [
+      "k8s-cloudcos-cloudcos-e1bd232905-1211250675.us-east-1.elb.amazonaws.com"
+    ],
+
     proxy: {
       "/api": {
         target: "http://cloudcostops-backend:8000",
